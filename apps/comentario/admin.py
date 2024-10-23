c4e361ee4bf225d93f53853afdea1aaba878990e
Magui Brollo <33180142+MaguiBrollo@users.noreply.github.com>
2024-10-23 09:05:32 -0300

    Revert "Develop - Publicación - Categoría - Comentario"

--- apps/comentario/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.comentario.models import Comentario
-
-# Register your models here.
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('publicacion', 'autor', 'texto')
-    list_filter = ('publicacion', 'autor')
-
-admin.site.register(Comentario, ComentarioAdmin)
--- apps/publicacion/admin.py
+++ apps/publicacion/admin.py
@@ -18,8 +18,7 @@ admin.site.register(Categoria, CategoriaAdmin)
 #-------------- PUBLICACION
 class PublicacionAdmin(admin.ModelAdmin) :
    list_display = ('id',
-                   'titulo_publicacion',
-                   'resumen', 
+                   'titulo_publicacion', 
                    'publicacion',
                    'imagen', 
                    'mostrar_imagen',
@@ -34,9 +33,17 @@ class PublicacionAdmin(admin.ModelAdmin) :
 admin.site.register(Publicacion,PublicacionAdmin)
 
 #-------------- COMENTARIOS
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('publicacion', 'autor', 'texto')
-    list_filter = ('publicacion', 'autor')
+class ComentarioAdmin(admin.ModelAdmin) :
+   list_display = ('id',
+                   'comentario', 
+                   'editado',
+                   'fecha_comentario', 
+                   'fecha_modificacion',
+                   'usuario',
+                   'publicacion')
+   
+   list_filter=('comentario','usuario')
+   search_fields=('comentario',)
    
 admin.site.register(Comentario,ComentarioAdmin)
 
