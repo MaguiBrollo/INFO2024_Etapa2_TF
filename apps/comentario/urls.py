90ea84852b456842de7785210fa3725c43ceec11
maxicoscia <maxicoscia@gmail.com>
2024-10-28 21:40:59 -0300

    botón eliminar comentario

--- apps/comentario/urls.py
+++ apps/comentario/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DetallePublicacionView, EditarComentarioView
+from .views import DetallePublicacionView, EditarComentarioView, EliminarComentarioView
 
 app_name = 'comentario'
 urlpatterns = [
     path('publicacion/<int:pk>/', DetallePublicacionView.as_view(), name='detalle_publicacion'),
     path('comentario/editar/<int:pk>/', EditarComentarioView.as_view(), name='editar_comentario'),
+    path('comentario/eliminar_comentario/<int:pk>/', EliminarComentarioView.as_view(), name='eliminar_comentario'),
     ] 
